b17ec0bc4b235b9e1cd656270a6e1bc3b574c4d1
vnhai <haingocvu@hotmail.com>
2018-06-12 14:08:49 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container mt30">
+                <div className="row">
+                    <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div className="panel panel-primary">
+                              <div className="panel-heading">
+                                    <h3 className="panel-title">Color Picker</h3>
+                              </div>
+                              <div className="panel-body">
+                                    Panel content
+                              </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div className="panel panel-warning">
+                              <div className="panel-heading">
+                                    <h3 className="panel-title">Size: 15px</h3>
+                              </div>
+                              <div className="panel-body">
+                                    <button type="button" className="btn btn-success">Increase</button>&nbsp;
+                                    <button type="button" className="btn btn-success">Decrease</button>
+                              </div>
+                        </div>
+                        <button type="button" className="btn btn-primary">Reset</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <p>Color: red - Font size: 15px</p>
+                        <div id="setting-content">Setting Content</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
